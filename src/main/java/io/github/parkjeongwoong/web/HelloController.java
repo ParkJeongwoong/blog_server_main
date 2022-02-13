ea6c4cb0e925d7100da68c69c277141e29f99748
dvlprjw <dvlprjw@gmail.com>
2022-02-13 19:59:00 +0900

    CORS Fix v2

--- src/main/java/io/github/parkjeongwoong/web/HelloController.java
+++ src/main/java/io/github/parkjeongwoong/web/HelloController.java
@@ -1,12 +1,10 @@
 package io.github.parkjeongwoong.web;
 
 import io.github.parkjeongwoong.web.dto.HelloResponseDto;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HelloController {
 
