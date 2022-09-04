bc0d225a09a1ca0e076a46f6d329e81902f534be
dvlprjw <dvlprjw@gmail.com>
2022-09-04 10:49:57 +0900

    Add Download Service

---
+++ src/main/java/io/github/parkjeongwoong/application/data/service/DataService.java
@@ -0,0 +1,60 @@
+package io.github.parkjeongwoong.application.data.service;
+
+import io.github.parkjeongwoong.application.data.usecase.DataUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+
+@RequiredArgsConstructor
+@Service
+public class DataService implements DataUsecase {
+    @Value("${download.path}")
+    String filePath;
+
+    // 출처 : https://kitty-geno.tistory.com/105
+    @Override
+    public void download(HttpServletRequest request, HttpServletResponse response, String filename) throws IOException {
+        if (filename == null || filename.equals("")) {
+            return ;
+        }
+
+        System.out.println(filePath);
+        System.out.println(filename);
+        File dFile = new File(filePath, filename);
+
+        int fSize = (int) dFile.length();
+
+        if (fSize > 0) {
+            String encodedFilename = "attachment; filename*=" + "UTF-8" + "''" + URLEncoder.encode(filename, "UTF-8");
+            response.setContentType("application/octet-stream; charset=utf-8");
+            response.setHeader("Content-Dispotition", encodedFilename);
+            response.setContentLengthLong(fSize);
+
+            BufferedInputStream in;
+            BufferedOutputStream out;
+
+            in = new BufferedInputStream(new FileInputStream(dFile));
+            out = new BufferedOutputStream(response.getOutputStream());
+
+            try {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+
+                out.flush();
+            } finally {
+                in.close();
+                out.close();
+            }
+        }
+
+    }
+}
